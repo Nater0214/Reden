fd68b79765882b3a909fdec8f937fc159d903305
Nater0214 <gamer@nater0214.com>
2023-03-18 17:33:50 -0400

    Add template app start script

--- main.py
+++ main.py
@@ -3,9 +3,29 @@
 # A LAN messaging platform
 
 
+# Imports
+import sys
+
+from PySide6 import QtWidgets
+
+import src.gui as gui
+
+
 # Definitions
 def main() -> None:
-    print("Reden")
+    """Main"""
+    
+    # Create Qt app
+    app = QtWidgets.QApplication(sys.argv)
+    
+    # Create the windows
+    gui.create_windows(app)
+    
+    # Show the start window
+    gui.start_window.show()
+    
+    # Start app and exit with code
+    sys.exit(app.exec())
 
 
 # Run
