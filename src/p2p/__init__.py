f06053286a1026c2b46c32914674ccf8853cd6dc
Nater0214 <gamer@nater0214.com>
2023-03-22 15:10:39 -0400

    Remove unused imports

--- src/p2p/__init__.py
+++ src/p2p/__init__.py
@@ -5,15 +5,9 @@
 
 
 # Imports
-import datetime
-import hashlib
-import json
-from os import mkdir
 from os.path import exists
 
-from getmac import getmac
 
-from . import exceptions
 from .src import my_node as my_node
 
 
