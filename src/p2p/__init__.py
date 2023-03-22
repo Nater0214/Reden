378fb6445cd9903f917f2c9bc9656a105b64892d
Nater0214 <gamer@nater0214.com>
2023-03-22 15:11:45 -0400

    Add basic node getting

--- src/p2p/__init__.py
+++ src/p2p/__init__.py
@@ -12,8 +12,22 @@ from .src import my_node as my_node
 
 
 # Definitions
+def get_nodes() -> dict | None:
+    """Get the nodes"""
+    
+    # Create the nodes file if it doesn't exist
+    if not exists("store/nodes.json"):
+        with open("store/nodes.json", 'wt') as file:
+            file.write("{}")
+        
+        return None
+
+
 def start() -> bool:
     """Start p2p"""
+    
+    # Get the nodes from the file
+    nodes = get_nodes()
     
     # Create local node
     local_node = my_node.LocalNode()
