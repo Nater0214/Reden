c680da0a025a4229311f01a91ee09e0d5f90a454
Nater0214 <gamer@nater0214.com>
2023-03-22 15:56:55 -0400

    Add StartWindow class

--- src/gui/__init__.py
+++ src/gui/__init__.py
@@ -2,6 +2,30 @@
 # Initializes the windows
 
 
+# Imports
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+from . import ui_start
+
 # Definitions
+
+# Window handlers
+class StartWindow(QMainWindow, ui_start.Ui_MainWindow):
+    def __init__(self, app: QApplication):
+        # Init
+        
+        super(StartWindow, self).__init__()
+        self.setupUi(self)
+        
+        self.app = app
+        
+        # Button connections
+        self.setupButton.clicked.connect(self.setup_ui_start)
+    
+    
+    def closeEvent(self, event) -> None:
+        self.app.quit()
+
+
 def create_windows() -> None:
     pass
