d2ac7eb84688f612eaca41eddb65cb910b19316a
Nater0214 <gamer@nater0214.com>
2023-04-10 11:23:19 -0400

    Add comments

--- src/p2p/src/my_node/_node.py
+++ src/p2p/src/my_node/_node.py
@@ -23,7 +23,8 @@ class LocalNode(Node):
             node_id = json_data["local-node"]["id"]
         else:
             node_id = hashlib.new("sha1", str(datetime.datetime.now()).encode()).hexdigest()
-            
+
+        # Initialize node
         super().__init__(socket.gethostbyname(socket.gethostname()), 56787, node_id)
 
 
@@ -33,4 +34,5 @@ class PeerNode(Node):
     def __init__(self, json_data: dict) -> None:
         """Create a connected peer node"""
         
+        # Initialize node
         super().__init__(json_data["ip"], 56787, json_data["id"])
