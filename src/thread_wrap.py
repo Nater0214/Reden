8d58950a2d59a4d2930e5be2b6db2bd1f93ac8c3
Nater0214 <gamer@nater0214.com>
2023-04-14 17:04:50 -0400

    Add a thread wrapper

---
+++ src/thread_wrap.py
@@ -0,0 +1,38 @@
+# src/thread_wrap.py
+# Contains a function wrapper that turns it into a thread
+
+
+# Imports
+from threading import Thread
+
+
+# Definitions
+def thread_wrap(thread_name: str):
+    """A function wrapper that turns it into a thread
+    Just decorate the function, and call it like normal!"""
+    
+    def decorator(func):
+        """The decorator itself"""
+        
+        class Wrapper(Thread):
+            """The wrapper itself"""
+
+            def __init__(self, func) -> None:
+                """Create the wrapper"""
+
+                # Set variables
+                self._func = func
+                self._thread_name = thread_name
+
+
+            def __call__(self, *args, **kwargs):
+
+                # Initialize the thread
+                super().__init__(target=self._func, name=self._thread_name, args=args, kwargs=kwargs)
+
+                # Start the thread
+                self.start()
+        
+        return Wrapper(func)
+    
+    return decorator
