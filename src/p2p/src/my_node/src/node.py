4eab84dec7d1251e83aaf49fd3e75960bcc0adb5
Nater0214 <gamer@nater0214.com>
2023-04-17 11:32:44 -0400

    Nodes can hopefully connect

--- src/p2p/src/my_node/src/node.py
+++ src/p2p/src/my_node/src/node.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 
 import datetime
 import hashlib
+from random import choice
 import socket
 
 from p2pnetwork.node import Node
@@ -16,13 +17,13 @@ from p2pnetwork.node import Node
 class LocalNode(Node):
     """The local node on this machine"""
 
-    def __init__(self, json_data: dict | None = None) -> None:
+    def __init__(self, node_json: dict) -> None:
         """Create a local node on this machine"""
         
         # Get id from json data, or use hash of current time if unavailable
-        if json_data["local-node"]:
-            node_id = json_data["local-node"]["id"]
-            node_port = json_data["local-node"]["port"]
+        if node_json["local-node"]:
+            node_id = node_json["local-node"]["id"]
+            node_port = node_json["local-node"]["port"]
         
         else:
             node_id = hashlib.new("sha1", str(datetime.datetime.now()).encode()).hexdigest()
@@ -31,21 +32,52 @@ class LocalNode(Node):
         self.ip = socket.gethostbyname(socket.gethostname())
 
         # Add known nodes to a list
-        if json_data["known-nodes"]:
-            self.known_nodes = json_data["known-nodes"]
+        if node_json["known-nodes"]:
+            self.known_nodes = node_json["known-nodes"]
 
         # Initialize node
         super().__init__(self.ip, node_port, node_id)
     
     
+    # Events
+    def inbound_node_connected(self, node: Node) -> None:
+        self.add_known_node(node)
+    
+    
+    def outbound_node_connected(self, node: Node) -> None:
+        self.add_known_node(node)
+    
+    
     # Methods
+    def add_known_node(self, node: Node) -> None:
+        self.known_nodes.append({
+            "ip": node.ip,
+            "port": node.port,
+            "id": node.node_id
+        })
+
+
     def connect_with_node(self, json_data: dict) -> bool:
         """Connect with a node"""
         
         return super().connect_with_node(json_data["ip"], json_data["port"])
+
+
+    def connect_random(self, amount: int = 1) -> None:
+        """Connect with an amount of random nodes"""
+        
+        for _ in range(amount):
+            self.connect_with_node(choice(self.known_nodes))
     
     
-    def disconnect_all(self):
+    def add_node(self, ip: str, port: int) -> None:
+        """Connect to a new node"""
+
+        # Attempt to connect to node
+        self.connect_with_node({"ip": ip, "port": port})
+    
+    
+    def disconnect_all(self) -> None:
         """Disconnect from every node"""
         
         for node in self.nodes_outbound:
