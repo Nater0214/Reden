9c8773325772d09109f220f85ef6607037a8d29a
Nater0214 <gamer@nater0214.com>
2023-04-25 15:37:25 -0400

    Remove all stuff

--- src/p2p/__init__.py
+++ src/p2p/__init__.py
@@ -1,18 +1,3 @@
 # src/p2p/__init__.py
 # Peer-to-peer stuff
 # Written by me
-
-
-# Imports
-import json
-from os import mkdir, path
-from pathlib import Path
-
-from .src import my_node as my_node
-
-
-# Variables
-local_node = None
-
-
-# Definition
