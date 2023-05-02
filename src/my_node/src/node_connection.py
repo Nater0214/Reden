97e0d264f22fdfb5a1914ced3c01ab23e492d1e5
Nater0214 <gamer@nater0214.com>
2023-05-02 11:18:32 -0400

    Move asking stuff to the NodeConnection

--- src/my_node/src/node_connection.py
+++ src/my_node/src/node_connection.py
@@ -9,3 +9,13 @@ import p2pnetwork.nodeconnection
 # Definitions
 class NodeConnection(p2pnetwork.nodeconnection.NodeConnection):
     """My override of the node connection"""
+    
+    
+    # Methods
+    def ask(self, for_: str) -> None:
+        """Ask the connection for something"""
+        
+        self.send({
+            "action": "ask",
+            "body": for_
+        })
