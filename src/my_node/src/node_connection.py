34fe939311cb9654b58c008f1fd67d7c0d5e3784
Nater0214 <gamer@nater0214.com>
2023-05-02 09:43:53 -0400

    Add class

---
+++ src/my_node/src/node_connection.py
@@ -0,0 +1,11 @@
+# src/my_node/src/node_connection.py
+# My override of the node connection
+
+
+# Imports
+import p2pnetwork.nodeconnection
+
+
+# Definitions
+class NodeConnection(p2pnetwork.nodeconnection.NodeConnection):
+    """My override of the node connection"""
