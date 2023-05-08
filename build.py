c86178e74a8ed3d2c640e24bf0ff47971da8d84d
Nater0214 <gamer@nater0214.com>
2023-05-08 11:51:01 -0400

    Update build steps

--- build.py
+++ build.py
@@ -4,9 +4,10 @@
 
 # Imports
 import os
+import py_compile
 import re
-from argparse import ArgumentParser
 import shutil
+from argparse import ArgumentParser
 
 import PyInstaller.__main__
 
@@ -25,11 +26,22 @@ def main(args):
         f"{name}.spec"
     ])
     
+    # Organize the output
     print("Organizing Output")
     files = list(filter(re.compile(r"^.*\.pyd$|^.*(?<!python\d)(?<!python\d{3})\.dll$").match, os.listdir(out_path := os.path.join(os.path.realpath(os.path.dirname(__file__)), "dist", name))))
     os.mkdir(lib_path := os.path.join(out_path, "lib"))
     for file in files:
         shutil.move(os.path.join(out_path, file), os.path.join(lib_path, file))
+        print(f"{os.path.join(out_path, file)} -> {os.path.join(lib_path, file)}")
+    
+    # Compile src folder
+    print("Compiling src")
+    for root, dirs, files in os.walk(os.path.join(os.path.realpath(os.path.dirname(__file__)), "dist", name, "src")):
+        for file in files:
+            if re.search(r".*\.py$", file_path := os.path.join(root, file)):
+                py_compile.compile(file_path, f"{file_path}c")
+                print(f"{file_path} -> {file_path}c")
+                os.remove(file_path)
 
 
 # Run
